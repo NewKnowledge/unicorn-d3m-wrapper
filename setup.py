be6d6b70f249ce48d1d55c42faed1aa6c05a7297
Numa Dhamani <numadhamani@gmail.com>
2019-01-15 18:11:51 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'd3m_unicorn >= 1.0.0'
     ],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/d3m_unicorn@924713009fbbc8d7b4c564f1106d46d6cda595c5#egg=d3m_unicorn-1.0.0"
+        "git+https://github.com/NewKnowledge/d3m_unicorn@32576e6d5abd5683afd34f79c48b9ad07fe8693a#egg=d3m_unicorn-1.0.0"
     ],
     entry_points={
         'd3m.primitives': [
