f924f4ed3c7f98c7605c4e4980fdb7cc25d74916
numadhamani <numadhamani@gmail.com>
2019-05-21 19:49:42 +0000

    update produce method to add denormalize

--- UNICORNd3mWrapper/unicorn_pipeline.py
+++ UNICORNd3mWrapper/unicorn_pipeline.py
@@ -6,6 +6,19 @@ from d3m.metadata.pipeline import Pipeline, PrimitiveStep
 pipeline_description = Pipeline(context=Context.TESTING)
 pipeline_description.add_input(name='inputs')
 
+# Step 0: Denormalize primitive
+step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.denormalize.Common'))
+step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
+step_0.add_output('produce')
+pipeline_description.add_step(step_0)
+
+# Step 1: dataset_to_dataframe
+step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.dataset_to_dataframe.Common'))
+step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
+step_1.add_hyperparameter(name='dataframe_resource', argument_type= ArgumentType.VALUE, data='learningData')
+step_1.add_output('produce')
+pipeline_description.add_step(step_1)
+
 # Step 2: Unicorn primitive
 step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.digital_image_processing.unicorn.Unicorn'))
 step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
