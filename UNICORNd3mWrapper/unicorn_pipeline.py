e821dc7d103c73cdb33ae1e6175607bbf4213d84
jgleason <jeffrey.gleason@newknowledge.io>
2019-08-12 16:38:44 -0500

    update pipeline to make unicorn last primitive

--- UNICORNd3mWrapper/unicorn_pipeline.py
+++ UNICORNd3mWrapper/unicorn_pipeline.py
@@ -26,34 +26,8 @@ step_2.add_hyperparameter(name='image_columns', argument_type= ArgumentType.VALU
 step_2.add_output('produce')
 pipeline_description.add_step(step_2)
 
-# Step 3: imputer -> imputes null values based on mean of column
-step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
-step_3.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE,data='replace')
-step_3.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE,data=True)
-pipeline_description.add_step(step_3)
-
-# Step 4,5,6: Distil ensemble random forest classifier
-step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon'))
-step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
-step_4.add_output('produce')
-pipeline_description.add_step(step_4)
-
-# extract target columns
-step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon'))
-step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
-step_5.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,data=('https://metadata.datadrivendiscovery.org/types/Target',))
-step_5.add_output('produce')
-pipeline_description.add_step(step_5)
-
-# extract feature columns
-step_6 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.learner.random_forest.DistilEnsembleForest'))
-step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
-step_6.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
-step_6.add_output('produce')
-pipeline_description.add_step(step_6)
-
 # Final Output
-pipeline_description.add_output(name='output predictions', data_reference='steps.6.produce')
+pipeline_description.add_output(name='output predictions', data_reference='steps.2.produce')
 
 # Output json pipeline
 blob = pipeline_description.to_json()
